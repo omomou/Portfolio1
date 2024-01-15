f8b2d88b6301aafc264b55c8cb084f4b70fbbfcd
omonou <156267218+omomou@users.noreply.github.com>
2024-01-14 20:02:01 -0600

    start

---
+++ java.js
@@ -0,0 +1,13 @@
+// Author: Aryan Pradhan 
+// Date: 2024-01-14
+
+console.log("javascript")
+
+let luckyNumber = 10;
+
+luckyNumber = luckyNumber * 5;
+
+let userFavoriteNumber = prompt("What is your lucky number?");
+
+console.log("Favorite Number:", luckyNumber);
+console.log("User's favorite number:", userFavoriteNumber);
