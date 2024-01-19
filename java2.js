2f72af5033a6a61a61f12248e75ec440fa42aa9b
omomou <156267218+omomou@users.noreply.github.com>
2024-01-19 06:35:30 -0600

    Add files via upload

---
+++ java2.js
@@ -0,0 +1,17 @@
+console.log("javascript2")
+
+let userRandomNumber = prompt("Give me a number!");
+
+var n = 7;
+
+var j = 0;
+
+while (j < 5 ) {
+    if (n % 2 !==0) {
+        console.log(n)
+    }
+
+    n++;
+
+    j++;
+}
